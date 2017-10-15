ae5ace546a6e7b55e3631dfd7b0397bb1b80c96b
Vaidotas Gedzius <vaidotas@gedzius.lt>
2017-10-15 11:49:02 +0100

    Added Run/Stop button

--- sketch.js
+++ sketch.js
@@ -6,10 +6,27 @@ var nCols = 20;
 var w = 10;
 var DEBUG = false;
 
+var runBtn;
+
+var running = false;
+
 function setup() {
-  frameRate(1);
+  frameRate(2);
   canvas = createCanvas(nRows * w + 1, nCols * w + 1);
   canvas.parent('canvas-container');
+
+  runBtn = select('#run');
+  runBtn.mousePressed(() => {
+    if (running) {
+      // noLoop();
+      running = false;
+      runBtn.html('Run');
+    } else {
+      // loop();
+      running = true;
+      runBtn.html('Stop');
+    }
+  });
 
   grid = new Grid(nRows, nCols);
   grid.cell(0, 0).alive = true;
@@ -22,5 +39,7 @@ function setup() {
 function draw() {
   background(255);
   grid.show();
-  grid.update();
+  if (running) {
+    grid.update();
+  }
 }
